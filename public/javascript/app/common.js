a2e71acd4be38897a46ffe7828767ad6c51846b7
vishnu <balavishnu.r@krds.fr>
2016-05-27 02:21:25 +0530

    notification mobile

--- public/javascript/app/common.js
+++ public/javascript/app/common.js
@@ -1,9 +1,14 @@
 var Common  =   function() {
     this.notificationPage   =   1;
     this.is_scroll_listner_active   =   false;
+    this.is_mobile  =   false;
 };
+var animationTimeout;
 
 Common.prototype.init   =   function() {
+    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) )
+        this.is_mobile  =   true;
+
     this.bindEvents();
 };
 
@@ -23,24 +28,51 @@ Common.prototype.bindEvents =   function() {
                 $('#notification_container').off('scroll');
             }
 
+            if(typeof animationTimeout !== undefined)
+                clearTimeout(animationTimeout);
+
             _this.notificationPage   =   1;
+            $('#main').show().removeClass('animate').addClass('moveBack');
         }
         else
         {
             $(this).addClass('active').parent().addClass('active');
             $(this).find('.badge').empty();
-            _this.getNotification();
+            $('#main').removeClass('moveBack').addClass('animate');
+            _this.getNotification(true);
             _this.setNotificationSeenStatus();
         }
     });
 
-    $('body').on('click', function(e) {
-        if ( ! $(e.target).hasClass('clickInActive') && $('.btnNotifications').hasClass('active'))
-           $('.btnNotifications').trigger('click');
-    })
+    $('#btn_notificaiton_back').on('click', function(e) {
+        e.preventDefault();
+
+        $('.btnNotifications').removeClass('active').parent().removeClass('active');
+        $('#notification_container').empty().scrollTop(0);
+        $('#block_notifications').hide();
+        if(_this.is_scroll_listner_active) {
+            _this.is_scroll_listner_active   =   false;
+            $('#notification_container').off('scroll');
+        }
+
+        _this.notificationPage   =   1;
+
+        if(typeof animationTimeout !== undefined)
+            clearTimeout(animationTimeout);
+
+        $('#main').show().removeClass('animate').addClass('moveBack');
+    });
+
+    if( ! this.is_mobile) {
+        $('body').on('click', function(e) {
+            if ( ! $(e.target).hasClass('clickInActive') && $('.btnNotifications').hasClass('active'))
+               $('.btnNotifications').trigger('click');
+        });
+    }
+
 };
 
-Common.prototype.getNotification =   function() {
+Common.prototype.getNotification =   function(is_animate) {
     var _this   =   this;
 
     if(_this.busy)
@@ -50,6 +82,19 @@ Common.prototype.getNotification =   function() {
     $('body').addClass('loading');
     $('#block_notifications').show();
     $('#notification_loader').show();
+
+    if($('#btn_notificaiton_back').is(':visible') && is_animate) {
+        $('html, body').scrollTop(0);
+        var height  =   $(window).height();
+        $('#notification_container').css({height: height});
+        if(typeof animationTimeout !== undefined)
+            clearTimeout(animationTimeout);
+
+        anmiationTimeout    =   setTimeout(function() {
+            if($('#main').hasClass('animate'))
+                $('#main').hide();
+        }, 1100);
+    }
 
     $.ajax({
         cache: false,
@@ -65,7 +110,6 @@ Common.prototype.getNotification =   function() {
         },
         success: function(data) {
             $('#notification_container').append(data.html);
-            $('#block_notifications').show();
 
             if(_this.notificationPage === 1)
                 Materialize.showStaggeredList('#notification_container');
@@ -94,7 +138,7 @@ Common.prototype.listenScroll   =   function() {
     $('#notification_container').on('scroll', function(e){
         if($(this).scrollTop() + $(this).innerHeight() >= $(this)[0].scrollHeight) {
             _this.notificationPage  += 1;
-            _this.getNotification();
+            _this.getNotification(false);
         }
     });
 }
